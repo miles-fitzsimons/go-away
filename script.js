e029bea22b53ef7d128f6d506de62194d86b4bbf
miles-fitzsimons <miles.fitzsimons@gmail.com>
2016-06-02 20:55:07 +1200

    logo fades in and out

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 	var navPos = $("nav").offset().top;
-	console.log("navPos = " + navPos);
 
 	$(window).scroll(function() {
 		var winPos = $(window).scrollTop();
@@ -8,18 +7,30 @@ $(document).ready(function() {
 		if (navPos < winPos) {
 			$("nav").css({"position": "fixed", "top": "0", "background-color": "#FFFFFF", "color": "#000000", "border-bottom": "solid 2px #D9D9D9"});
 			$(".intro").css("height", "100vh");
+			$("#navLogo").css("visibility", "visible");
+			$("#navLogo").fadeIn();
+			// $("#navContainer").css({"position": "fixed", "top": 0, "width": "100%"})
 		}
 		// If page scrolls above nav bar
 		else {
 			$(".intro").css("height", "85vh");
+			// $("#navLogo").css("visibility", "hidden")
+			$("#navLogo").fadeOut();
+
 			var r = winPos / navPos;
-			console.log("winPos = " + winPos + "  navPos = " + navPos + "  r = " + r);
 			var fontColor = Math.floor((1 - r) * 255);
 			fontColor = fontColor.toString(16);
 			var bgColor = Math.floor(r * 256);
 			bgColor = bgColor.toString(16);
-			console.log("bgColor = " + bgColor + "  fontColor = " + fontColor);
 			$("nav").css({"position": "initial", "top": "initial", "border-bottom": "none", "background-color": "#"+bgColor+bgColor+bgColor, "color": "#"+fontColor+fontColor+fontColor});
 		}
 	});
+
+	// $("li").hover(function() {
+	// 	$(this).css("border-bottom", "solid 1px #FFFFFF");
+	// },
+	// );
+
+
+
 });
